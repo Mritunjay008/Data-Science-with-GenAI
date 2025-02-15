f4e96a53dbae51eef6b267ab553a268ab89b8518
Mritunjay kumar <121124935+Mritunjay008@users.noreply.github.com>
2025-02-15 21:48:48 +0530

    Add files via upload

---
+++ Python/Module06/library_package_modules_import_statement/teacher/tempCodeRunnerFile.py
@@ -0,0 +1,11 @@
+import os, sys
+from os.path import dirname, abspath, join
+parent_path = abspath(join(dirname(__file__), '..'))
+sys.path.insert(0, parent_path)
+from student import student_details
+student_details.student()
+
+
+
+def teacher():
+    print("This is teacher details")
